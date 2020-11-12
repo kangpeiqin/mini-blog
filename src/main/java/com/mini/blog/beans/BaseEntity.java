1ed5d11557022eca142cd5174e04ef94848d9a81
kangpeiqin <1060647301@qq.com>
2020-11-13 00:14:29 +0800

    代码自动生成构建

---
+++ src/main/java/com/mini/blog/beans/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.mini.blog.beans;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/**
+ * @author kpq
+ * @since 1.0.0
+ */
+@Data
+@ApiModel(value = "实体类公共字段", description = "实体类公共字段")
+public class BaseEntity implements Serializable {
+    @ApiModelProperty("主键")
+    private Long id;
+    @ApiModelProperty("创建时间")
+    private LocalDate createTime;
+    @ApiModelProperty("更新时间")
+    private LocalDate updateTime;
+}
